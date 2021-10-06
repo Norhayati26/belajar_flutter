b1f23dfaaf37fa3174627c9b050c4dec961797b2
norhayati26 <ayaanorhayati26@gmail.com>
2021-10-06 10:53:45 +0800

    Fist Commit : Praktik Video 3

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Belajar Flutter',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Hello AppBar', 
+        style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        leading: Icon(Icons.dehaze),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.search, color: Colors.black),
+            onPressed: null,
+          ),
+          IconButton(
+            icon: const Icon(Icons.more_vert, color: Colors.black),
+            onPressed: null,
+          )
+        ]
+      ),
+    );
+  }
+}
